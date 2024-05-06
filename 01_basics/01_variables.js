debe0bc1abb1c8f2e5c989f9a6db6106117ce951
sanjay Bakoliya <115345049+Sanjay022@users.noreply.github.com>
2024-05-06 12:57:30 +0000

    variable and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const addressId = 1211121;
+
+let Name = "sanjay";
+var isLoggedIn = true;
+addressName = "null"
+
+
+// addressId = 11  // it sould be defined 
+
+console.table([addressId, Name, isLoggedIn, addressName])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,9 @@
+const name = "snajay";
+
+let age = 21;
+let isLoggedIn = "null"
+let table ;
+
+
+
+console.log(typeof isLoggedIn);
